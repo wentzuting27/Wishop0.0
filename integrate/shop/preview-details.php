e237b13d0e20f87319ebd2db2da933746a6dbad7
Taro <yutinglu506@gmail.com>
2024-05-10 21:57:16 +0800

    預告改樣子跟預告詳情頁

---
+++ integrate/shop/preview-details.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>開團預告</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Sailor
+  * Template URL: https://bootstrapmade.com/sailor-free-bootstrap-theme/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+
+  <script src="https://kit.fontawesome.com/2956805ca8.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="page-portfolio">
+<?php session_start(); ?>
+   <!-- ======= Header ======= -->
+   <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="../index/index.php"></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.php" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="../index/index.php" class="active">首頁</a></li>
+          <li class="dropdown"><a href="../index/portfolio.php"><span>購物</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="about.php">About</a></li>
+              <li><a href="team.php">Team</a></li>
+              <li><a href="testimonials.php">Testimonials</a></li>
+
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li><a href="#">團購</a></li>
+          <li><a href="../wish/wish.php">許願池</a></li>
+
+          <?php
+            if(!empty($_SESSION['user_name'])){
+              echo '
+              <li><a href="#"><i class="fa-solid fa-bell"></i></a></li>
+
+              <li class="dropdown"><a href="../profile/Profile_settings.php"><img src="',$_SESSION["user_avatar"],'" class="nav-photo"></a>
+                <ul>
+                  <li><a style="color:#FFF;font-weight: 600;margin-bottom: 0px;">',$_SESSION["user_name"],'</a></li>
+                  <hr>
+                  <li><a href="../profile/Wishlist.php" style="font-weight: 600;">收藏清單</a></li>
+                  <li><a href="../profile/Purchase_history.php" style="font-weight: 600;">購買紀錄</a></li>
+                  <li><a href="../index/logout.php" style="font-weight: 600;">登出&nbsp;<i class="fa-solid fa-right-from-bracket"></i></a></li>
+                </ul>
+              </li>
+              ';
+            }else{
+              echo "<a href='../index/login.php' class='getstarted' style='color: white;'>登入</a>";
+            }
+          ?>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+    <?php
+      $link=mysqli_connect('localhost','root','12345678','wishop');
+      $preview_id=$_GET["preview_id"];
+      $shop_id=$_GET["shop_id"];
+      $sql="select *
+      from preview
+      where preview_id=$preview_id";
+      $result=mysqli_query($link,$sql);
+      while($row=mysqli_fetch_assoc($result))
+      {
+        $preview_title=$row["preview_title"];
+        $preview_narrate=$row["preview_narrate"];
+        $time=$row["time"];
+      }
+    ?>
+    <section id="wish-details-hero">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url(https://i.pinimg.com/564x/bf/f3/7e/bff37e041a3114c7b318665ff2de4964.jpg)">
+            <div class="carousel-container">
+              <div class="container">
+                <h2 class="animate__animated animate__fadeInDown" ><?php echo $preview_title; ?></h2>               
+              </div>
+            </div>
+          </div>
+  
+      </div>
+    </section><!-- End Hero -->
+    
+
+
+
+    <!-- ======= Blog Single Section ======= -->
+    <section id="blog" class="blog">
+      <div class="container" data-aos="fade-up">
+
+        <section id="breadcrumbs" class="breadcrumbs">
+          <div class="container">
+            <div class="d-flex justify-content-between align-items-center">
+              <ol>
+                <li><a href="shop_time.php?shop_id=<?php echo $shop_id; ?>">返回賣場</a></li>
+                <li>預告詳情</li>
+              </ol>
+              
+            </div>
+          </div>
+        </section><!-- End Breadcrumbs -->
+
+        <div class="row gy-4">
+
+          <div class="col-lg-5 entries">
+
+            <article class="entry">
+
+              <div class="entry-img">
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                  <div class="carousel-inner fixed-image2">
+                    <?php
+                    $a=1;
+                    $sql_photo="select *
+                    from preview_photo
+                    where preview_id='$preview_id'";
+                    $result_photo=mysqli_query($link,$sql_photo);
+                    while($row_photo=mysqli_fetch_assoc($result_photo))
+                    {
+                      echo '
+                    <div class="carousel-item ';if($a==1){echo 'active"';}echo '">
+                      <img src="',$row_photo["preview_photo_link"],'" class="d-block w-100" alt="...">
+                    </div>';
+                    $a++;
+                    }
+                    ?>
+                    
+                  </div>
+                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                  </button>
+                </div>
+              </div>             
+
+            </article><!-- End blog entry -->
+
+
+          </div><!-- End blog entries list -->
+
+          <div class="col-lg-7">
+            <section id="wish-details" class="wish-details"> 
+              <div class="container" data-aos="fade-up">    
+                <div class="wish-info">
+                  <div class="wish-details-title">
+                    <h3>預告資訊</h3>
+                  </div>
+                  <br>
+                  <ul>
+                    <li><strong><i class="bi bi-clock"></i>&nbsp;發布日期</strong>: <?php echo $time; ?></li>
+                    <li><strong><i class="bi bi-chat-dots"></i>&nbsp;預告敘述</strong>: </li>
+                      <p class="pre_scrollable-row"><?php echo nl2br($preview_narrate); ?></p>
+                  </ul>
+                </div>              
+              </div>
+            </section>  
+
+
+          </div><!-- End blog sidebar -->
+
+      </div>
+    </section><!-- End Blog Single Section -->
+
+  </main><!-- End #main -->
+
+
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
