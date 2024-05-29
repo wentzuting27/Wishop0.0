a443871450e9e9aa76e72b32e0cf4adbffb6c14b
Sumi <lalaye62@gmail.com>
2024-05-29 15:17:01 +0800

    先把檢舉的兩個頁面丟上來 還沒偷成功

---
+++ integrate/shop/rr_details.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Portfolio Details - Sailor Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- icon -->
+  <script src="https://kit.fontawesome.com/ca83b2e10a.js" crossorigin="anonymous"></script>
+
+  <!-- =======================================================
+  * Template Name: Sailor
+  * Template URL: https://bootstrapmade.com/sailor-free-bootstrap-theme/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+  <?php session_start(); ?>
+  <main id="main">
+
+
+
+
+    <!-- ======= Portfolio Details Section ======= -->
+    <section id="portfolio-details" class="portfolio-details">
+      <div class="tab-content row justify-content-center" data-aos="fade-up" data-aos-delay="200">
+
+        <!-- 5star -->
+        <div role="tabpanel" class="col-lg-12 tab-pane fade show active" id="5star">
+          <div class="row">
+
+            <div class="col-lg-4">
+
+              <div class="evaluate_card">
+                <table class="evaluate_table">
+                  <tr>
+                    <td width="15%"><img src="https://i.pinimg.com/564x/f6/1a/e4/f61ae484b7c56295adbaa4010235cd68.jpg"
+                        class="people_photo"></td>
+                    <td width="60%">
+                      <span>Taro_0106</span><br>
+                      <p>東京-排球少年×三麗鷗聯名快閃店</p>
+                    </td>
+                    <td width="25%" style="vertical-align: top;" align="right"><i
+                        class="fa-solid fa-wand-sparkles"></i><i class="fa-solid fa-wand-sparkles"></i><i
+                        class="fa-solid fa-wand-sparkles"></i><i class="fa-solid fa-wand-sparkles"></i><i
+                        class="fa-solid fa-wand-sparkles"></i></td>
+                  </tr>
+                  <tr>
+                    <td colspan="3">
+                      <div class="scrollable-row">
+                        這次的代購經驗真是讓我非常滿意！商品包裝精美，完好無損地送達，而且速度快得令人驚訝。代購商的服務態度也非常好，及時回覆我的疑問並提供了專業的建議。下次有需要我一定會再次光顧！</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colspan="3" style="height: 70px;"><img
+                        src="https://img.ws.mms.shopee.tw/tw-11134211-7qukx-li51ahq6fgz8d8" class="goods_photo"><img
+                        src="https://img.ws.mms.shopee.tw/tw-11134211-7qukx-li51ahq6fgz8d8" class="goods_photo"><img
+                        src="https://img.ws.mms.shopee.tw/tw-11134211-7qukx-li51ahq6fgz8d8" class="goods_photo"><img
+                        src="https://img.ws.mms.shopee.tw/tw-11134211-7qukx-li51ahq6fgz8d8" class="goods_photo"><img
+                        src="https://i.pinimg.com/564x/f5/fa/ad/f5faadb7550f067819e859b62c3dd784.jpg"
+                        class="goods_photo"><img
+                        src="https://i.pinimg.com/236x/2a/c8/b6/2ac8b69c8f02d00e2a17fa0202cc68d5.jpg"
+                        class="goods_photo"><img
+                        src="https://i.pinimg.com/236x/6b/c3/a7/6bc3a791734a08b8428b99586cbda1bd.jpg"
+                        class="goods_photo"></td>
+                  </tr>
+                </table>
+              </div>
+
+            </div>
+
+            <div class="col-lg-4">
+
+              <div class="evaluate_card">
+                <table class="evaluate_table">
+                  <tr>
+                    <td width="15%"><img src="https://i.pinimg.com/564x/1f/c6/6a/1fc66a08447b965a3e1000ccfc784029.jpg"
+                        class="people_photo"></td>
+                    <td width="60%">
+                      <span>Taro_0106</span><br>
+                      <p>東京-排球少年×三麗鷗聯名快閃店</p>
+                    </td>
+                    <td width="25%" style="vertical-align: top;" align="right"><i
+                        class="fa-solid fa-wand-sparkles"></i><i class="fa-solid fa-wand-sparkles"></i><i
+                        class="fa-solid fa-wand-sparkles"></i><i class="fa-solid fa-wand-sparkles"></i><i
+                        class="fa-solid fa-wand-sparkles"></i></td>
+                  </tr>
+                  <tr>
+                    <td colspan="3">
+                      <div class="scrollable-row">
+                        這次代購真是太棒了！商品質量超出我的預期，與描述完全一致。包裝非常用心，沒有任何損壞或破損。而且代購商的配送速度也很快，讓我感到非常滿意。強烈推薦給所有需要的朋友！</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colspan="3" style="height: 70px;"><img
+                        src="https://img.ws.mms.shopee.tw/tw-11134211-7qukx-li51ahq6fgz8d8" class="goods_photo"></td>
+                  </tr>
+                </table>
+              </div>
+
+            </div>
+
+            <div class="col-lg-4">
+
+              <div class="evaluate_card">
+                <table class="evaluate_table">
+                  <tr>
+                    <td width="15%"><img src="https://i.pinimg.com/564x/60/cf/43/60cf43172ea279f05b95318ee79e6afe.jpg"
+                        class="people_photo"></td>
+                    <td width="60%">
+                      <span>Taro_0106</span><br>
+                      <p>東京-排球少年×三麗鷗聯名快閃店</p>
+                    </td>
+                    <td width="25%" style="vertical-align: top;" align="right"><i
+                        class="fa-solid fa-wand-sparkles"></i><i class="fa-solid fa-wand-sparkles"></i><i
+                        class="fa-solid fa-wand-sparkles"></i><i class="fa-solid fa-wand-sparkles"></i><i
+                        class="fa-solid fa-wand-sparkles"></i></td>
+                  </tr>
+                  <tr>
+                    <td colspan="3">
+                      <div class="scrollable-row">非常感謝</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colspan="3" style="height: 70px;"></td>
+                  </tr>
+                </table>
+              </div>
+
+            </div>
+          </div>
+        </div>
+    </section><!-- End Portfolio Details Section -->
+
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
