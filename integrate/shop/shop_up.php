5570663eeb6b621e2dc805a4e9c1bd4f1e2c3366
Taro <yutinglu506@gmail.com>
2024-06-02 21:58:41 +0800

    每一頁的編輯賣場、修改上傳照片的程式碼

--- integrate/shop/shop_up.php
+++ integrate/shop/shop_up.php
@@ -2,6 +2,7 @@
     session_start(); date_default_timezone_set('Asia/Taipei');
 
     $shop_id = $_POST["shop_id"];
+    $page = $_POST["page"];
     $shop_name = $_POST["shop_name"];
     $shop_narrat = $_POST["shop_narrat"];
 
@@ -12,20 +13,22 @@
 
     if (!empty($_FILES['shop_avatar']['name'])) {
     
-        if ($_FILES['shop_avatar']['error'] == UPLOAD_ERR_OK){
+        if ($_FILES['shop_avatar']['error'] == UPLOAD_ERR_OK) {
             $file = $_FILES['shop_avatar']['tmp_name'];
-            $shop_avatar_dest = '../files/' . $_FILES['shop_avatar']['name'];
-    
+            $originalName = pathinfo($_FILES['shop_avatar']['name'], PATHINFO_FILENAME);
+            $extension = pathinfo($_FILES['shop_avatar']['name'], PATHINFO_EXTENSION);
+            $shop_avatar_dest = '../files/' . $originalName . '.' . $extension;
+        
             // 檢查是否有一樣名字的檔案
             if (file_exists($shop_avatar_dest)) {
                 $i = 1;
-                while (file_exists('../files/' . $_FILES['shop_avatar']['name'] . '_' . $i)) {
+                while (file_exists('../files/' . $originalName . '_' . $i . '.' . $extension)) {
                     $i++;
                 }
-                $shop_avatar_dest = "../files/" . $_FILES['shop_avatar']['name'] . '_' . $i;
+                $shop_avatar_dest = '../files/' . $originalName . '_' . $i . '.' . $extension;
             }
-    
-            //將檔案移至指定位置
+        
+            // 將檔案移至指定位置
             move_uploaded_file($file, $shop_avatar_dest);
         }
     }else{
@@ -39,20 +42,22 @@
 
     if (!empty($_FILES['shop_bg']['name'])) {
     
-        if ($_FILES['shop_bg']['error'] == UPLOAD_ERR_OK){
+        if ($_FILES['shop_bg']['error'] == UPLOAD_ERR_OK) {
             $file = $_FILES['shop_bg']['tmp_name'];
-            $shop_bg_dest = '../files/' . $_FILES['shop_bg']['name'];
-    
+            $originalName = pathinfo($_FILES['shop_bg']['name'], PATHINFO_FILENAME);
+            $extension = pathinfo($_FILES['shop_bg']['name'], PATHINFO_EXTENSION);
+            $shop_bg_dest = '../files/' . $originalName . '.' . $extension;
+        
             // 檢查是否有一樣名字的檔案
             if (file_exists($shop_bg_dest)) {
                 $i = 1;
-                while (file_exists('../files/' . $_FILES['shop_bg']['name'] . '_' . $i)) {
+                while (file_exists('../files/' . $originalName . '_' . $i . '.' . $extension)) {
                     $i++;
                 }
-                $shop_bg_dest = "../files/" . $_FILES['shop_bg']['name'] . '_' . $i;
+                $shop_bg_dest = '../files/' . $originalName . '_' . $i . '.' . $extension;
             }
-    
-            //將檔案移至指定位置
+        
+            // 將檔案移至指定位置
             move_uploaded_file($file, $shop_bg_dest);
         }
     }else{
@@ -65,10 +70,10 @@
         where shop_id='$shop_id'";
 
     if (mysqli_query($link, $sql)) {
-        echo '<script>alert("賣場資訊編輯成功!"); window.history.go(-1);</script>';
+        echo '<script>alert("賣場資訊編輯成功!");window.location.href = "' . $page . '.php?shop_id=' . $shop_id . '";</script>';
         exit(); 
     } else{
-        echo '<script>alert("賣場資訊編輯失敗");  window.history.go(-1);</script>'; 
+        echo '<script>alert("賣場資訊編輯失敗"); window.location.href = "' . $page . '.php?shop_id=' . $shop_id . '";</script>'; 
     }
     
     
