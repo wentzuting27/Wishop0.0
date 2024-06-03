04280ad81d896c522954bc2bb2a278a666f527f0
Sumi <lalaye62@gmail.com>
2024-06-03 18:26:30 +0800

    改

--- integrate/profile/editPassword.php
+++ integrate/profile/editPassword.php
@@ -1,47 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-        <?php
+    <?php
 
 
-$link = mysqli_connect("localhost", "root", "12345678", "wishop");
+    $link = mysqli_connect("localhost", "root", "12345678", "wishop");
 
-$current_password = $_POST['current_password'];
-$new_password = $_POST['new_password'];
-$confirm_password = $_POST['confirm_password'];
+    $current_password = $_POST['current_password'];
+    $new_password = $_POST['new_password'];
+    $confirm_password = $_POST['confirm_password'];
 
-$account = $_SESSION['account'];
+    $account = $_SESSION['account'];
 
-$sql = "SELECT password FROM account WHERE account='$account'";
-$result = mysqli_query($link, $sql);
-$row = mysqli_fetch_assoc($result);
-$stored_password = $row['password'];
+    $sql = "SELECT password FROM account WHERE account='$account'";
+    $result = mysqli_query($link, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $stored_password = $row['password'];
 
-if ($current_password === $stored_password) {
-    // 輸入的密碼是否等於原本舊的密碼
-    if ($new_password === $confirm_password) {
-        // 新密碼是否等於二次確認的密碼
-        $update_sql = "UPDATE account SET password='$new_password' WHERE account='$account'";
-        // 編輯密碼
-        mysqli_query($link, $update_sql);
+    if ($current_password === $stored_password) {
+        // 輸入的密碼是否等於原本舊的密碼
+        if ($new_password === $confirm_password) {
+            // 新密碼是否等於二次確認的密碼
+            $update_sql = "UPDATE account SET password='$new_password' WHERE account='$account'";
+            // 編輯密碼
+            mysqli_query($link, $update_sql);
 
-        echo '<script>alert("密碼修改成功！"); window.history.go(-1);</script>';
+            echo '<script>alert("密碼修改成功！"); window.history.go(-1);</script>';
             exit();
+        } else {
+            // 再次輸入的密碼與新密碼不相符
+            echo '<script>alert("再次輸入的密碼與新密碼不相符，請檢查是否輸入錯誤！"); window.history.go(-1);</script>';
+            exit();
+        }
     } else {
-        // 再次輸入的密碼與新密碼不相符
-        echo '<script>alert("再次輸入的密碼與新密碼不相符，請檢查是否輸入錯誤！"); window.history.go(-1);</script>';
+        // 輸入的密碼不等於原本舊的密碼
+        echo '<script>alert("原密碼輸入錯誤，請輸入正確密碼！"); window.history.go(-1);</script>';
         exit();
     }
-} else {
-    // 輸入的密碼不等於原本舊的密碼
-    echo '<script>alert("原密碼輸入錯誤，請輸入正確密碼！"); window.history.go(-1);</script>';
-    exit();
-}
 
 
 
@@ -53,24 +55,7 @@ if ($current_password === $stored_password) {
 
 
 
-
-
-        // $method = $_POST["method"];
-        // $account = $_POST["account"];
-        // $password = $_POST["password"];
-
-        // $link = mysqli_connect("localhost", "root", "12345678", "wishop");
-        // if ($method == "update") {
-        //     $sql = "update account set password='$password'
-        //     where account='{$_SESSION["account"]}'";
-        //     if (mysqli_query($link, $sql)) {
-        //         header("refresh:0;url=Profile_settings.php");
-        //     } else {
-        //         header("refresh:0;url=Profile_settings.php");
-        //     }
-
-        // } else {
-        // }
-        ?>
+    ?>
 </body>
+
 </html>
